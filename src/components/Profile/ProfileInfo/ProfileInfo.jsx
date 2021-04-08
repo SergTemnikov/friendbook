8d4ce1170133ff2bcefebbf579086bff0e570caf
Sergey Temnikov <sergey.t@devstark.com>
2021-04-08 22:46:33 +0300

    fix the longest bug ever...

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -5,16 +5,23 @@ import Preloader from "../../common/Preloader/preloader";
 const ProfileInfo = (props) => {
     if (!props.profile) {
         return <Preloader />
+    } else {
+        console.log(props);
     }
 
     return (
         <div>
-            <div>
+            {/* <div>
                 <img className={s.topBg} src='https://pbs.twimg.com/media/DFwTsX2VoAIC2UJ.jpg'/>
-            </div>
+            </div> */}
             <div className={s.descriptionBlock}>
             <img src={props.profile.photos.large}/>
-                {props.profile}
+                <div>
+                    {props.profile.aboutMe}
+                </div>
+                <div>
+                    {props.profile.fullName}
+                </div>
             </div>
         </div>
     )
